33223ecc48815ed33ae8ffd7d022c9b42c7765c4
jillallan <jillallan24@gmail.com>
2020-05-01 13:24:12 +0100

    added configuration to init

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(os.environ['APP_SETTINGS'])
 
     @app.route('/')
     def hello():
         return "Hello world"
+
+    print(os.environ['APP_SETTINGS'])
 
     return app
